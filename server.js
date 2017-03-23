a933f28e13e797082c2e6702e9f2b4bf9db384d2
wsadmin <wsadmin>
2017-03-23 11:50:42 -0400

    fixed bug - 3/23/17

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ as well as the data that delivers from the front to the back-end and vice verse.
 var express = require("express");
 var app = express(); //* we have a variable set to the express library
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080; //* we set a variable to our PORT 8080
 
@@ -24,7 +25,7 @@ var jsonParser = bodyParser.json()*/
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //parse various different custom JSON types as JSON using bodyParser app
-app.use(bodyParser.json({ type: "application/++json"}))
+app.use(bodyParser.json({ type: "application/*+json"}))
 
 //parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
