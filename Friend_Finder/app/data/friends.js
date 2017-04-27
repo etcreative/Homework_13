ea9741518d19ecba284574380976bdc0fcb45e05
wsadmin <wsadmin>
2017-04-27 19:12:48 -0400

    FF app working now

---
+++ Friend_Finder/app/data/friends.js
@@ -0,0 +1,117 @@
+var friends = [
+
+{
+	name: "Paul",
+	photo: "https://pbs.twimg.com/profile_images/769172064175583232/HC2dX_Fj.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+{
+	name: "Bethany",
+	photo: "http://static5.businessinsider.com/image/54d395a76da811710354bbef-1190-625/heres-what-sports-illustrated-swimsuit-models-look-like-in-real-life.jpg",
+	scores: [
+	"4",
+	"5",
+	"4",
+	"4",
+	"5",
+	"1",
+	"2",
+	"1",
+	"2",
+	"1"
+	]
+},
+{
+	name: "Max",
+	photo: "http://flowjournal.org/wp-content/uploads/2012/04/Ran-Sarig.png",
+	scores: [
+	"1",
+	"2",
+	"5",
+	"4",
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+},
+{
+	name: "Mark",
+	photo: "https://upload.wikimedia.org/wikipedia/ru/8/80/Rich_Mullins_black_and_white_short_hair.jpg",
+	scores: [
+	"1",
+	"1",
+	"4",
+	"4",
+	"5",
+	"5",
+	"2",
+	"4",
+	"4",
+	"1"
+	]
+},
+{
+	name: "Teddy",
+	photo: "http://www.worthytales.net/wp-content/uploads/2014/07/desktop-1406690442.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"4",
+	"4",
+	"4",
+	"1"
+	]
+},
+{
+	name: "Casey",
+	photo: "http://www.hipgirlie.com/wp-content/uploads/2016/11/how-to-take-selfie.jpg",
+	scores: [
+	"5",
+	"1",
+	"4",
+	"4",
+	"5",
+	"1",
+	"5",
+	"1",
+	"4",
+	"1"
+	]
+},
+{
+	name: "Wofgang",
+	photo: "http://abunawaf.com/wp-content/uploads/2015/09/%D8%B3%D9%85%D9%83%D8%A9.jpg",
+	scores: [
+	"5",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1",
+	"2",
+	"5",
+	"4",
+	"1"
+	]
+}
+];
+
+module.exports = friends;
--- Friend_Finder/server.js
+++ Friend_Finder/server.js
@@ -11,17 +11,17 @@ as well as the data that delivers from the front to the back-end and vice verse.
 
 //* dependencies - npm packages "express", "bodyParser"
 var express = require("express");
-var app = express(); //* we have a variable set to the express library
+var app = express();//* we have a variable set to the express library
 var bodyParser = require("body-parser");
 var path = require("path");
 
-var PORT = process.env.PORT || 8080; //* we set a variable to our PORT 8080
+var PORT = process.env.PORT || 8080;//* we set a variable to our PORT 8080
 
 /*create application/JSON parser
 var jsonParser = bodyParser.json()*/
 
-
 //create application/x-www-form-urlencoded parser
+/*(bodyParser - below)helps body parser translate the communication from the back to the front-end */
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //parse various different custom JSON types as JSON using bodyParser app
@@ -39,4 +39,3 @@ require("./app/routing/html-routes.js")(app);
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
-
